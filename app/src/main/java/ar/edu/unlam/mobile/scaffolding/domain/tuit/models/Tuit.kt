9c8812b36aa29f900ab88efa1d9675984f53dd40
Maximiliano De Pietro <maximiliano.deprieto@mercadolibre.com>
2024-10-01 20:18:51 -0300

    :construction: Tuit Model

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/tuit/models/Tuit.kt
@@ -0,0 +1,12 @@
+package ar.edu.unlam.mobile.scaffolding.domain.tuit.models
+
+data class Tuit(
+    val id: Int,
+    val authorName: String,
+    val content: String,
+    val avatar: String,
+    val likes: Int,
+    val liked: Boolean,
+    val replies: Int,
+    val reply: (id: Int) -> Void,
+)
