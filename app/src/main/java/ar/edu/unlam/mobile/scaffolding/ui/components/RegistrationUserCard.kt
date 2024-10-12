c6ea1c2d57a886d603deef4d6d49ba298a92293b
Elena Juárez <elenajuarez32@gmail.com>
2024-10-12 19:24:18 -0300

    creacion de formulario de registro

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/RegistrationUserCard.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/RegistrationUserCard.kt
@@ -1,10 +1,33 @@
 package ar.edu.unlam.mobile.scaffolding.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Magenta
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ar.edu.unlam.mobile.scaffolding.domain.tuit.models.Tuit
 
 
@@ -13,6 +36,59 @@ import ar.edu.unlam.mobile.scaffolding.domain.tuit.models.Tuit
 fun RegistrationUserCard() {
 
     Card() {
-        Text("bienvenido")
-    }
+Column(
+
+    modifier = Modifier.fillMaxSize().padding(16.dp),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally,
+
+
+){
+
+        val gradientColors = listOf(Cyan, Blue, Magenta /*...*/)
+        var email by remember { mutableStateOf("") }
+        var name by remember { mutableStateOf("") }
+        var pass by remember { mutableStateOf("") }
+        Text(
+            "REGISTRARSE",
+
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
+            modifier = Modifier.width(200.dp),
+            style = TextStyle(
+                brush = Brush.linearGradient(
+                    colors = gradientColors
+                )
+            )
+        )
+
+    Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("E-mail") }
+        )
+
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Nombre") }
+        )
+
+        OutlinedTextField(
+            value = pass,
+            onValueChange = { pass = it },
+            label = { Text("Contraseña") }
+        )
+    Spacer(modifier = Modifier.height(16.dp))
+        OutlinedButton(onClick = { onClick() }) {
+            Text("Enviar")
+        }
+
+    }}
+}
+
+fun onClick() {
+    TODO("Not yet implemented")
 }
