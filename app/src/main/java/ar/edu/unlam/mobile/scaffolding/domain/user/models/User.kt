f9c6c9cf30228f82f02e7140f6ac008ff85ffe84
Elena Juárez <elenajuarez32@gmail.com>
2024-10-12 12:48:32 -0300

    creacion de model user

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/user/models/User.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/user/models/User.kt
@@ -1,4 +1,10 @@
 package ar.edu.unlam.mobile.scaffolding.domain.user.models
 
-class User {
-}
+data class User (
+
+    val avatarUrl: String,
+    val email: String,
+    val name: String,
+    val password: String
+
+    )
