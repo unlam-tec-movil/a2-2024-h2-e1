e29b9035eec7424b159d7d558e71003e41790516
Elena Juárez <elenajuarez32@gmail.com>
2024-10-12 13:32:57 -0300

    creacion de card para registrar usuario

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/RegistrationUserCard.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/RegistrationUserCard.kt
@@ -1,4 +1,18 @@
 package ar.edu.unlam.mobile.scaffolding.ui.components
 
-class RegistrationUserCard {
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ar.edu.unlam.mobile.scaffolding.domain.tuit.models.Tuit
+
+
+@Composable
+@Preview
+fun RegistrationUserCard() {
+
+    Card() {
+        Text("bienvenido")
+    }
 }
