ac66738a62e4d79de5a42267da82011f61da9650
Elena Juárez <elenajuarez32@gmail.com>
2024-10-13 11:15:36 -0300

    solucionando errores de ktlint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/user/models/User.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/user/models/User.kt
@@ -1,9 +1,7 @@
 package ar.edu.unlam.mobile.scaffolding.domain.user.models
 
-data class User (
-
+data class User(
     val email: String,
     val name: String,
     val password: String,
-
 )
--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/RegistrationUserCard.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/RegistrationUserCard.kt
@@ -12,13 +12,9 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-//import androidx.compose.runtime.mutableStateOf
-//import androidx.compose.runtime.remember
-//import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-//import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Cyan
 import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.Magenta
