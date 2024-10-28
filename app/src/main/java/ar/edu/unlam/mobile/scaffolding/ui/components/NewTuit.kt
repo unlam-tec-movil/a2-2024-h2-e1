f928d948fb1a88ce88fe3276f8fc7e4fdeabb746
ivancarranza12 <82419685+ivancarranza12@users.noreply.github.com>
2024-10-28 20:28:42 -0300

    feature/components
    Se agregaron componentes

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/NewTuit.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/NewTuit.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -61,4 +62,10 @@ fun NewTuit() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun NewTuitPreview(){
+    NewTuit()
 }
