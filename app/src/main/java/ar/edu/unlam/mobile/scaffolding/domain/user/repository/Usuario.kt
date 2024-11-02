e1af3775c4c11389390288d136b2e245634b8bdd
juanchi-apk <juanmanuelotero83@gmail.com>
2024-11-02 14:25:33 -0300

    agregada rama facu

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/user/repository/Usuario.kt
+++
@@ -1,8 +0,0 @@
-package ar.edu.unlam.mobile.scaffolding.domain.user.repository
-
-data class Usuario(
-    val nombre:String,
-    val username:String,
-    val tuits:Int,
-    val mensajes:String
-)
