b3ed8e5578cb47251650b94c6e6e5705362b8904
ivancarranza12 <82419685+ivancarranza12@users.noreply.github.com>
2024-11-03 11:45:51 -0300

    feature/feed-home

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/TuitFeed.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/TuitFeed.kt
@@ -1,23 +1,53 @@
 package ar.edu.unlam.mobile.scaffolding.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import ar.edu.unlam.mobile.scaffolding.domain.tuit.models.Tuit
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TuitFeed(
     tuits: List<Tuit>,
     likeEvent: (id: Int) -> Unit,
+
 ) {
-    var modifier = Modifier.fillMaxSize()
-    LazyColumn(modifier.padding(top = 10.dp)) {
-        items(tuits) { tuit ->
-            TuitCard(likePost = likeEvent, tuit = tuit, modifier = Modifier.padding(1.dp))
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column {
+            // Top Bar
+            TopAppBar(
+                title = { Text("Twitter Unlam") },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surface
+                )
+            )
+            var modifier = Modifier.fillMaxSize()
+
+            LazyColumn(modifier.padding(top = 10.dp)) {
+                item {
+                    TweetComposer()
+                }
+                items(tuits) { tuit ->
+                    TuitCard(likePost = likeEvent, tuit = tuit, modifier = Modifier.padding(1.dp))
+                }
+            }
         }
     }
 }
